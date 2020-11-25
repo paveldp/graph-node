fddea23b4f3a4150fc0a5fbd8b678f548f05bb04
David Lutterkort <lutter@watzmann.net>
2020-11-24 16:06:18 -0800

    store: Have Store.block_ptr take a reference to the deployment

--- graph/src/data/subgraph/mod.rs
+++ graph/src/data/subgraph/mod.rs
@@ -927,7 +927,7 @@ impl Graft {
             vec![SubgraphManifestValidationError::GraftBaseInvalid(msg)]
         }
 
-        match store.block_ptr(self.base.clone()) {
+        match store.block_ptr(&self.base) {
             Err(e) => gbi(e.to_string()),
             Ok(None) => gbi(format!(
                 "failed to graft onto `{}` since it has not processed any blocks",
